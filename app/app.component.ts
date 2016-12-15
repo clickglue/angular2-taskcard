045bcee9e32245895e967ea49362ca3872749e88
clickglue <clickglue@telenet.be>
2016-12-15 22:30:08 +0100

    donderdag

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,17 @@
 import {Component} from '@angular/core';
+import {Task  } from './model/task';
 
 @Component({
     moduleId:module.id,
     selector:'my-app',
-    templateUrl:'app.component.html'
+    templateUrl:'app.component.html',
+    styleUrls:['app.component.css']
 })
 
-export class AppComponent {}
+export class AppComponent {
+    private tasks=[
+        new Task("Buy a monkey",false),
+        new Task("Walk the turtle",false),
+        new Task("Help", false),
+    ]
+}
